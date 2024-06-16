a9672f4025d54ca5a59f8983983439f3e3c166cb
zaid-bashir <zaidwani2282@gmail.com>
2024-06-16 15:25:48 +0530

    Readme.md Added

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import {config} from 'dotenv';
 const app = express();
+
+//Loading Config.env File Path To Start Loading .env File Variables.
 config({path : './config/config.env'})
 
 export default app;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import app from "./app.js";
 
+//Listening To Server
 app.listen(process.env.PORT,()=>{
     console.log(`Server Listening On Port ${process.env.PORT}`);
 });
