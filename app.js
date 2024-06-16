198b7c975513566863a62259afdef21382dd01ad
zaid-bashir <zaidwani2282@gmail.com>
2024-06-16 15:42:02 +0530

    Added CORS

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 import express from 'express';
-import {config} from 'dotenv';
+import { config } from 'dotenv';
+import cors from 'cors';
 const app = express();
 
 //Loading Config.env File Path To Start Loading .env File Variables.
-config({path : './config/config.env'})
+config({ path: './config/config.env' })
+
+//Adding CORS For Connecting FrontEnd and Dashboard With Backend.
+app.use(cors(
+    {
+        origin: [process.env.FRONTEND_URL, process.env.DASHBOARD_URL],
+        methods: ['GET', 'PUT', 'POST', 'DELETE', 'PATCH'],
+        credentials: true,
+    }
+));
 
 export default app;
